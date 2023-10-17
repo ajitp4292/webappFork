3972c52d7ac617ef682db1e72b7a3c3e569cc85e
ajitp4292 <patil.aji@northeastern.edu>
2023-10-17 01:02:44 -0400

    packer code & workflow

--- packer/start.sh
+++ packer/start.sh
@@ -9,4 +9,12 @@ sudo apt install -y postgresql postgresql-contrib
 # Install Node.js and npm
 sudo apt install -y nodejs npm
 
+# Set PostgreSQL default user password 
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Admin7914';"
+
+# Navigate to /opt directory
+cd /opt
+
+# Install unzip
+sudo apt install -y unzip
 
